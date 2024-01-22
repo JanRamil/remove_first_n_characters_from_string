fcfd67360d6f89e8bdcf5c3c3efbf0bf70055b52
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-22 23:54:11 +0800

    Creating the starting code including the remove_first_n_characters-from_string

--- remove_first_n_characters_from_string.py
+++ remove_first_n_characters_from_string.py
@@ -3,3 +3,9 @@
 
 # pseudocode
 
+# Creating the starting code including the def remove_first_n_characters_from_string
+def remove_first_n_characters_from_string(input_string, n):
+    if n >= 0:
+        return input_string[n:]
+    else:
+        return "Error: Please provide a non-negative value for n."
